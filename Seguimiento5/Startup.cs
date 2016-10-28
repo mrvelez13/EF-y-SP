a621387b9f50405bcc65b8c9a03601c916dda878
mrvel <mrvel@JONATHAN-PERSON>
2016-10-28 06:50:47 -0500

    Add project files.

---
+++ Seguimiento5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Seguimiento5.Startup))]
+namespace Seguimiento5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
